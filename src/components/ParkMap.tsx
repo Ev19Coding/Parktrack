acd4171780daeeccd4273d3839d029853f3837ae
Iceman8911 <wuchiJSS3@gmail.com>
2025-08-14 21:02:48 +0100

    fix: fixed issues related to the dynamic routes not working as expected

--- src/components/ParkMap.tsx
+++ src/components/ParkMap.tsx
@@ -2,15 +2,13 @@ import { useNavigate } from "@solidjs/router";
 import L from "leaflet";
 import { onMount } from "solid-js";
 import "leaflet/dist/leaflet.css";
-import type { JSX } from "solid-js";
-import { ESCAPED_HTML_ELEMENT_FROM_OUTER_HTML } from "~/shared/constants";
+import type { ParkRouteState } from "~/types/park-route";
 import { parks } from "../data/parks";
 
-
 const userLocationIcon = L.icon({
-  iconUrl: "https://img.icons8.com/officel/80/marker.png",
-  iconSize: [40, 40],
-  iconAnchor: [20, 40]
+	iconUrl: "https://img.icons8.com/officel/80/marker.png",
+	iconSize: [40, 40],
+	iconAnchor: [20, 40],
 });
 
 export default function ParkMap() {
@@ -20,49 +18,50 @@ export default function ParkMap() {
 	onMount(() => {
 		if (!mapDiv) return;
 
-    const map = L.map(mapDiv);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+		const map = L.map(mapDiv);
+		L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			attribution:
+				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		}).addTo(map);
 
-    const bounds = L.latLngBounds([]);
+		const bounds = L.latLngBounds([]);
 
-    parks.forEach((park) => {
-      const popupContent = `
+		parks.forEach(({ address, entryFee, id, lat, lng, name, phone }) => {
+			const popupContent = `
         <div style="width:200px">
-          <strong>${park.name}</strong><br/>
-          <em>${park.address}</em><br/>
-          Entry Fee: ${park.entryFee}<br/>
-          Phone: ${park.phone}
+          <strong>${name}</strong><br/>
+          <em>${address}</em><br/>
+          Entry Fee: ${entryFee}<br/>
+          Phone: ${phone}
         </div>
       `;
 
-      const marker = L.marker([park.lat, park.lng])
-        .addTo(map)
-        .bindPopup(popupContent);
+			const marker = L.marker([lat, lng]).addTo(map).bindPopup(popupContent);
 
-      // Double-click marker → go to park details
-      marker.on("dblclick", () => {
-        navigate(`/park/${park.id}`);
-      });
+			// Double-click marker → go to park details
+			const state: ParkRouteState = {
+				name,
+			};
+			marker.on("dblclick", () => {
+				navigate(`/park/${name}`, { state });
+			});
 
-      bounds.extend([park.lat, park.lng]);
-    });
+			bounds.extend([lat, lng]);
+		});
 
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(({ coords }) => {
-        const userMarker = L.marker([coords.latitude, coords.longitude])
-          .addTo(map)
-          .bindPopup("You are here");
+		if ("geolocation" in navigator) {
+			navigator.geolocation.getCurrentPosition(({ coords }) => {
+				const userMarker = L.marker([coords.latitude, coords.longitude])
+					.addTo(map)
+					.bindPopup("You are here");
 
-        bounds.extend(userMarker.getLatLng());
-        map.fitBounds(bounds, { padding: [50, 50] });
-      });
-    } else {
-      map.fitBounds(bounds, { padding: [50, 50] });
-    }
-  });
+				bounds.extend(userMarker.getLatLng());
+				map.fitBounds(bounds, { padding: [50, 50] });
+			});
+		} else {
+			map.fitBounds(bounds, { padding: [50, 50] });
+		}
+	});
 
-  return <div ref={mapDiv} style={{ height: "500px", width: "100%" }} />;
+	return <div ref={mapDiv} style={{ height: "500px", width: "100%" }} />;
 }
