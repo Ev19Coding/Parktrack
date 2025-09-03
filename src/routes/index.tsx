19d028a8fb95ed58b51967aa89c8866f9b1c781a
Iceman8911 <wuchiJSS3@gmail.com>
2025-09-03 18:22:17 +0100

    style: cleanup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,14 @@
 import { clientOnly } from "@solidjs/start";
-import { parkdata } from "~/data/parkdata";
 import SearchableParks from "../components/search";
+
 const ParkMapClient = clientOnly(() => import("../components/ParkMap"));
 
-
 export default function Home() {
-  return (
-    <div>
+	return (
+		<div>
+			<SearchableParks />
 
-      <SearchableParks />
-      
-      <ParkMapClient />
-    </div>
-    
-  );
+			<ParkMapClient />
+		</div>
+	);
 }
-
-
