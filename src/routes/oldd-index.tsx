04b4c35ecdb0a1e37462341ea74bb58de73c7e7f
Iceman8911 <wuchiJSS3@gmail.com>
2025-09-09 20:44:43 +0100

    wip: forgot to comment the route out >~<

--- src/routes/oldd-index.tsx
+++ src/routes/oldd-index.tsx
@@ -1,14 +1,13 @@
-import { clientOnly } from "@solidjs/start";
-import SearchableParks from "../components/search";
+// import { clientOnly } from "@solidjs/start";
 
-const ParkMapClient = clientOnly(() => import("../components/ParkMap"));
+// const ParkMapClient = clientOnly(() => import("../components/ParkMap"));
 
-export default function Home() {
-	return (
-		<div class="size-full flex flex-col justify-around items-center">
-			<SearchableParks />
+// export default function Home() {
+// 	return (
+// 		<div class="size-full flex flex-col justify-around items-center">
+// 			<SearchableParks />
 
-			<ParkMapClient />
-		</div>
-	);
-}
+// 			<ParkMapClient />
+// 		</div>
+// 	);
+// }
