d1c0470bbbf83b1593e60d8866ff3ee56ecf4ffa
Iceman8911 <wuchiJSS3@gmail.com>
2025-09-09 16:58:58 +0100

    wip: remove nav component
    i see no use of it ngl

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,14 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
 import "./app.css";
 
 export default function App() {
 	return (
 		<Router
 			root={(props) => (
-				<div class="w-screen h-screen flex flex-col">
-					<Nav />
-					<div class="grow">
-						<Suspense>{props.children}</Suspense>
-					</div>
+				<div class="w-screen h-screen overflow-clip">
+					<Suspense>{props.children}</Suspense>
 				</div>
 			)}
 		>
