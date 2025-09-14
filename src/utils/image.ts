4c940ff2c3442d0fda4220d8955534f2709ce8a4
Iceman8911 <wuchiJSS3@gmail.com>
2025-09-14 02:26:32 +0100

    fix(utils): don't proxy the placeholder image

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,4 +1,7 @@
+import PlaceholderImg from "~/assets/img/placeholder.webp";
 /** Will perform some edits to make the browser properly cache external image requests */
 export function getProxiedImageUrl(originalImgUrl: string) {
+	if (originalImgUrl === PlaceholderImg) return PlaceholderImg;
+
 	return `/api/image-proxy?url=${encodeURIComponent(originalImgUrl)}`;
 }
