246bbe80991d47eb4d33774b29a52a29af3d1c43
Yvonne Aderogba <yvonnaderogba18@gmail.com>
2025-09-17 23:37:17 +0100

    how to commit everything to a duplicated directory?

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,7 +1,7 @@
 import { PLACEHOLDER_IMG } from "~/shared/constants";
 /** Will perform some edits to make the browser properly cache external image requests */
-export function getProxiedImageUrl(originalImgUrl: string) {
+export function getProxiedImageUrl(originalImgUrl: string){
 	if (originalImgUrl === PLACEHOLDER_IMG) return PLACEHOLDER_IMG;
 
-	return `/api/image-proxy?url=${encodeURIComponent(originalImgUrl)}`;
+	return originalImgUrl;
 }
