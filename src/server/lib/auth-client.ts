d297db4c46397f4fc7cc010f642e2ac11a53c83c
Iceman8911 <wuchiJSS3@gmail.com>
2025-09-20 22:16:47 +0100

    wip(auth): export auth client

---
+++ src/server/lib/auth-client.ts
@@ -0,0 +1,6 @@
+import { createAuthClient } from "better-auth/solid"
+
+export const AUTH_CLIENT = createAuthClient({
+    /** The base URL of the server (optional if you're using the same domain) */
+    // baseURL: "http://localhost:3000"
+})
