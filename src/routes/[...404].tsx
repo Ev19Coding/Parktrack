335d26cad56b3b9e23f8db3e08835e982e031d6a
Iceman8911 <wuchiJSS3@gmail.com>
2025-09-27 20:58:15 +0100

    feat: make 404 page redirect better
    the catch all route now redirects the user back to their most recent
    page, if available, instead of always booting them back to the login
    page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,8 +1,15 @@
 import { useNavigate } from "@solidjs/router";
+import { onMount } from "solid-js";
 
 export default function NotFound() {
-	// Navigate back to the login page
-	useNavigate()("/");
+	// Navigate back to the previous page or login page
+	onMount(() => {
+		if (window.history.length > 1) {
+			window.history.back();
+		} else {
+			useNavigate()("/");
+		}
+	});
 
 	return "";
 }
