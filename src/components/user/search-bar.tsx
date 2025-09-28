d7d5fdb82f615c0f5541478ac2b96d3aefe61304
Iceman8911 <wuchiJSS3@gmail.com>
2025-09-28 02:11:37 +0100

    style(search-bar): remove unused code

--- src/components/user/search-bar.tsx
+++ src/components/user/search-bar.tsx
@@ -1,23 +1,13 @@
-import { A, createAsyncStore, query, useNavigate } from "@solidjs/router";
+import { A, createAsyncStore } from "@solidjs/router";
 import SearchIcon from "lucide-solid/icons/search";
 import { createSignal, Index, Show, Suspense } from "solid-js";
 import { useThrottle } from "solidjs-use";
 import { getProxiedImageUrl } from "~/utils/image";
-import {
-	queryRecreationalLocationById,
-	queryUserSearchForRecreationalLocations,
-} from "~/utils/user-query";
-import LoadingSpinner from "../loading-spinner";
+import { queryUserSearchForRecreationalLocations } from "~/utils/user-query";
 
 export default function UserSearchBar() {
-	const navigate = useNavigate();
-
 	const [input, setInput] = createSignal("");
 	const [areSuggestionsOpen, setAreSuggestionsOpen] = createSignal(false);
-	const [
-		isLoadingRecreationalLocationInfo,
-		setIsLoadingRecreationalLocationInfo,
-	] = createSignal(false);
 
 	// const { coords } = useGeolocation({ enableHighAccuracy: true });
 
@@ -34,9 +24,6 @@ export default function UserSearchBar() {
 		return _throttledSearch();
 	});
 
-	const getRecreationalLocationFromDatabaseById = (id: string) =>
-		queryRecreationalLocationById(id);
-
 	// Move ref declaration outside
 	let inputRef: HTMLInputElement | undefined;
 
@@ -48,85 +35,79 @@ export default function UserSearchBar() {
 	}
 
 	return (
-		<>
-			<details
-				class="dropdown place-self-center lg:col-[1/3]"
-				open={areSuggestionsOpen()}
-				onFocusOut={(e) => {
-					// Close the dropdown if focus moves to an element outside of the current dropdown.
-					if (!e.currentTarget.contains(e.relatedTarget as Node | null)) {
-						setAreSuggestionsOpen(false);
-					}
-				}}
+		<details
+			class="dropdown place-self-center lg:col-[1/3]"
+			open={areSuggestionsOpen()}
+			onFocusOut={(e) => {
+				// Close the dropdown if focus moves to an element outside of the current dropdown.
+				if (!e.currentTarget.contains(e.relatedTarget as Node | null)) {
+					setAreSuggestionsOpen(false);
+				}
+			}}
+		>
+			<summary class="block">
+				{/* Search bar */}
+				<label class="input bg-base-200 sm:min-w-120">
+					<SearchIcon class="text-base-content/50" />
+					<input
+						type="search"
+						placeholder="Search parks..."
+						value={input()}
+						onFocus={() => setAreSuggestionsOpen(true)}
+						onClick={() => setAreSuggestionsOpen(true)}
+						// Update query but keep dropdown open
+						onInput={setInputWithValue}
+						onKeyPress={({ key }) => key === "Enter" && setInputWithValue()}
+						class="flex-1 p-2 outline-none"
+						ref={inputRef}
+					/>
+				</label>
+			</summary>
+
+			{/* Dropdown. It's z-index is 1001 so that it stays above the leaflet map buttons */}
+			<ul
+				class="menu dropdown-content mt-2 max-h-96 w-full flex-nowrap overflow-y-auto rounded-box border bg-base-300 p-2 shadow-sm"
+				style={{ "z-index": 1001 }}
 			>
-				<summary class="block">
-					{/* Search bar */}
-					<label class="input bg-base-200 sm:min-w-120">
-						<SearchIcon class="text-base-content/50" />
-						<input
-							type="search"
-							placeholder="Search parks..."
-							value={input()}
-							onFocus={() => setAreSuggestionsOpen(true)}
-							onClick={() => setAreSuggestionsOpen(true)}
-							// Update query but keep dropdown open
-							onInput={setInputWithValue}
-							onKeyPress={({ key }) => key === "Enter" && setInputWithValue()}
-							class="flex-1 p-2 outline-none"
-							ref={inputRef}
-						/>
-					</label>
-				</summary>
+				{/* Wrap suspenses right around any stuff relying on `createAsync` */}
+				<Suspense>
+					<Index
+						each={[...(basicSearchResults.latest ?? [])]}
+						fallback={<div class="px-3 py-2">No results found</div>}
+					>
+						{(park) => {
+							return (
+								<li>
+									<A
+										type="button"
+										class="flex justify-between"
+										href={`/info/${park().id}`}
+										onClick={() => setAreSuggestionsOpen(false)}
+									>
+										{park().title}
 
-				{/* Dropdown. It's z-index is 1001 so that it stays above the leaflet map buttons */}
-				<ul
-					class="menu dropdown-content mt-2 max-h-96 w-full flex-nowrap overflow-y-auto rounded-box border bg-base-300 p-2 shadow-sm"
-					style={{ "z-index": 1001 }}
-				>
-					{/* Wrap suspenses right around any stuff relying on `createAsync` */}
-					<Suspense>
-						<Index
-							each={[...(basicSearchResults.latest ?? [])]}
-							fallback={<div class="px-3 py-2">No results found</div>}
-						>
-							{(park) => {
-								return (
-									<li>
-										<A
-											type="button"
-											class="flex justify-between"
-											href={`/info/${park().id}`}
-											onClick={() => setAreSuggestionsOpen(false)}
-										>
-											{park().title}
+										<Show when={park().thumbnail}>
+											{(thumbnail) => {
+												const proxiedImgUrl = () =>
+													getProxiedImageUrl(thumbnail());
 
-											<Show when={park().thumbnail}>
-												{(thumbnail) => {
-													const proxiedImgUrl = () =>
-														getProxiedImageUrl(thumbnail());
-
-													return (
-														<img
-															alt={park().title}
-															class="aspect-square h-7"
-															src={proxiedImgUrl()}
-															loading="lazy"
-														/>
-													);
-												}}
-											</Show>
-										</A>
-									</li>
-								);
-							}}
-						</Index>
-					</Suspense>
-				</ul>
-			</details>
-
-			<Show when={isLoadingRecreationalLocationInfo()}>
-				<LoadingSpinner />
-			</Show>
-		</>
+												return (
+													<img
+														alt={park().title}
+														class="aspect-square h-7"
+														src={proxiedImgUrl()}
+														loading="lazy"
+													/>
+												);
+											}}
+										</Show>
+									</A>
+								</li>
+							);
+						}}
+					</Index>
+				</Suspense>
+			</ul>
+		</details>
 	);
 }
