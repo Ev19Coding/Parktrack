8a375fc29c93995aa29b59592e8da674f5464512
Iceman8911 <wuchiJSS3@gmail.com>
2025-10-10 20:57:35 +0100

    fix(sql): workaround to the scraper's columns
    cus atm, the scraper uses an outdated schema

--- src/server/database/migrations/003_set_recreational_location_tables.sql
+++ src/server/database/migrations/003_set_recreational_location_tables.sql
@@ -35,3 +35,108 @@ CREATE TABLE IF NOT EXISTS "user_recreational_locations" (
   updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   isActive BOOLEAN NOT NULL DEFAULT TRUE
 );
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS id BIGINT;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS title VARCHAR;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS category VARCHAR DEFAULT 'Other';
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS address VARCHAR DEFAULT 'N/A';
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS link VARCHAR;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS latitude DOUBLE;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS longitude DOUBLE;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS thumbnail VARCHAR;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS images JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS openHours JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS popularTimes JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS description TEXT;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS phone VARCHAR;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS website VARCHAR;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS emails JSON DEFAULT '[]';
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS rating NUMERIC;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS reviewRating NUMERIC;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS reviewCount INTEGER;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS reviewsBreakdown JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS reviewsPerRating JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS priceRange VARCHAR;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS timezone VARCHAR;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS plusCode VARCHAR;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS dataId VARCHAR;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS reviewsLink VARCHAR;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS reservations JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS orderOnline JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS menu JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS owner JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS about JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS userReviews JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS userReviewsExtended JSON;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE user_recreational_locations
+ADD COLUMN IF NOT EXISTS isActive BOOLEAN DEFAULT TRUE;
